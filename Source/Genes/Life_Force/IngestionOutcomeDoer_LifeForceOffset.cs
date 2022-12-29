3c9e97beb61503838c771f88cae0beb4eb22d120
Shabakur <unkwownsecret@gmail.com>
2022-12-29 20:42:39 +0100

    Sexperience cum now gives fertilin

---
+++ Source/Genes/Life_Force/IngestionOutcomeDoer_LifeForceOffset.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace RJW_Genes
+{
+    public class IngestionOutcomeDoer_LifeForceOffset : IngestionOutcomeDoer
+    {
+		protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested)
+		{
+			if (GeneUtility.HasLifeForce(pawn))
+            {
+				float num = ingested.stackCount * this.FertilinPerUnit / 100;
+				GeneUtility.OffsetLifeForce(pawn, num);
+			}
+		}
+		public float FertilinPerUnit = 1f;
+	}
+}
