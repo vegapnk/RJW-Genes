f1e14befbb087eaa958bd423acc447d350ccd644
Shabakur <unkwownsecret@gmail.com>
2023-01-27 21:30:30 +0100

    First Draft naked prowess and racegenedef bugfix

---
+++ Source/Genes/Life_Force/Abilities/CompProperties_CasterIsNaked.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace RJW_Genes
+{
+	public class CompProperties_CasterIsNaked : CompProperties_EffectWithDest
+	{
+		public CompProperties_CasterIsNaked()
+		{
+			this.compClass = typeof(CompAbilityEffect_CasterIsNaked);
+		}
+	}
+}
+
