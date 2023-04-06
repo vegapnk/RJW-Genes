9a90026f80572241ac95a9625f333c8682dac2c3
Vegapnk <vegapnk@protonmail.com>
2023-04-06 08:24:00 +0200

    Changed youth fountain checks to fix #26 and fix #28.

--- Source/Genes/Special/Patch_AgeDrain.cs
+++ Source/Genes/Special/Patch_AgeDrain.cs
@@ -14,18 +14,14 @@ namespace RJW_Genes.Genes.Special
         /**
          * Update Issue #26: 
          * There are options that a 16 yo pawn and a 16 yo pawn have sex, 
-         * or there are races that have a different age-limits. 
-         * I don't want to account for "trust me this race is really adult with Age 6!!!"-stuff.
-         * As it is somewhat a bug when the pawns age tho, I added that the youth-fountain also needs to have MIN_AGE. 
-         * If you'd like a different behaviour, you have to do it yourself. 
+         * or there are races that have a different age-limits.
+         * I am not sure how I feel about this, but as some people that I consider "normal" asked me about this I changed it as requested in #26 and #28
          */
 
         const long AGE_TRANSFERED = 120000; // 120k == 2 days
         // 18 Years * 60 Days / Year * 60k Ticks/Day + 1 for safety
         const long MINIMUM_AGE = 18 * 60 * 60000 + 1;
 
-        // Comment AGE_TRANSFERED in for debugging, changes years
-        // const long AGE_TRANSFERED = 12000000; 
         public static void Postfix(SexProps props)
         {
             if (props == null || props.pawn == null || props.partner == null || props.partner.IsAnimal() )
@@ -39,8 +35,9 @@ namespace RJW_Genes.Genes.Special
 
                 // Make Partner older
                 props.partner.ageTracker.AgeBiologicalTicks += AGE_TRANSFERED;
-                // Make Pawn younger
-                props.pawn.ageTracker.AgeBiologicalTicks = Math.Max(MINIMUM_AGE, (pawnAge - AGE_TRANSFERED));
+                // Make Pawn younger if he is older than minimum age
+                if (pawnAge - AGE_TRANSFERED > MINIMUM_AGE)
+                    props.pawn.ageTracker.AgeBiologicalTicks = Math.Max(MINIMUM_AGE, (pawnAge - AGE_TRANSFERED));
             }
 
         }
--- Source/Genes/Special/Patch_Youth_Fountain.cs
+++ Source/Genes/Special/Patch_Youth_Fountain.cs
@@ -14,19 +14,14 @@ namespace RJW_Genes.Genes.Special
         /**
          * Update Issue #26: 
          * There are options that a 16 yo pawn and a 16 yo pawn have sex, 
-         * or there are races that have a different age-limits. 
-         * I don't want to account for "trust me this race is really adult with Age 6!!!"-stuff.
-         * As it is somewhat a bug when the pawns age tho, I added that the youth-fountain also needs to have MIN_AGE. 
-         * If you'd like a different behaviour, you have to do it yourself. 
+         * or there are races that have a different age-limits.
+         * I am not sure how I feel about this, but as some people that I consider "normal" asked me about this I changed it as requested in #26 and #28
          */
-
 
         const long AGE_REDUCTION = 60000; // 60k == 1 day
         // 18 Years * 60 Days / Year * 60k Ticks/Day + 1 for safety
         const long MINIMUM_AGE = 18 * 60 * 60000 + 1;
 
-        // Comment Below in for debugging
-        // const long AGE_REDUCTION = 6000000; // 6000k == 100 days
         public static void Postfix(SexProps props)
         {
             if (props == null || props.pawn == null || props.partner == null || props.partner.IsAnimal())
@@ -37,15 +32,12 @@ namespace RJW_Genes.Genes.Special
             {
                 var partnerAge = props.partner.ageTracker.AgeBiologicalTicks;
 
-                props.partner.ageTracker.AgeBiologicalTicks = Math.Max(MINIMUM_AGE, partnerAge - AGE_REDUCTION);
+                if(partnerAge - AGE_REDUCTION > MINIMUM_AGE)
+                    props.partner.ageTracker.AgeBiologicalTicks = Math.Max(MINIMUM_AGE, partnerAge - AGE_REDUCTION);
             }
 
         }
 
-        private static float ticksToYears(long ticks)
-        {
-            return (ticks / 60000f) / 60f;
-        }
     }
 
 }
