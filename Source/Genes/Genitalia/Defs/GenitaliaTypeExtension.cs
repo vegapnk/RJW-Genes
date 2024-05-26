a5c8843faf5186a6feea83492e69fb1673a414a2
a flock of birds <97801845+flock-of-birds@users.noreply.github.com>
2024-05-27 00:04:24 +0100

    Switched default GenitaliaTypeExtension parts from 'generic' to 'average'.

--- Source/Genes/Genitalia/Defs/GenitaliaTypeExtension.cs
+++ Source/Genes/Genitalia/Defs/GenitaliaTypeExtension.cs
@@ -9,15 +9,12 @@ namespace RJW_Genes
     public class GenitaliaTypeExtension : DefModExtension
     {
         private HediffDef_PartBase penis;
-        public HediffDef_PartBase Penis => penis ?? (HediffDef_PartBase) Genital_Helper.generic_penis;
+        public HediffDef_PartBase Penis => penis ?? (HediffDef_PartBase) Genital_Helper.average_penis;
 
         private HediffDef_PartBase vagina;
-        public HediffDef_PartBase Vagina => vagina ?? (HediffDef_PartBase) Genital_Helper.generic_vagina;
+        public HediffDef_PartBase Vagina => vagina ?? (HediffDef_PartBase) Genital_Helper.average_vagina;
 
         private HediffDef_PartBase anus;
-        public HediffDef_PartBase Anus => anus ?? (HediffDef_PartBase) Genital_Helper.generic_anus;
-
-        private HediffDef_PartBase breasts;
-        public HediffDef_PartBase Breasts => breasts ?? (HediffDef_PartBase) Genital_Helper.generic_breasts;
+        public HediffDef_PartBase Anus => anus ?? (HediffDef_PartBase) Genital_Helper.average_anus;
     }
 }
